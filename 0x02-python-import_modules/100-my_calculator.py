c7517e74b5eeac05775f83eadf2b3f40707a11f3
Freedom <freedom@localhost.localdomain>
2022-06-02 23:32:12 +0300

    advanced

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
     import sys
-    import calculator_1 as calc
-    ops = ["+", "-", "*", "/"]
-    l = sys.argv
-    if len(l) != 4:
-        (print("{}"
-         .format("Usage: ./100-my_calculator.py <a> <operator> <b>")))
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if not l[2] in ops:
-        (print("{}"
-         .format("Unknown operator. Available operators: +, -, * and /")))
+    operator = sys.argv[2]
+    if operator not in '*+-/':
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    a = int(l[1])
-    b = int(l[3])
-    op = l[2]
-    result = (calc.add(a, b)
-              if op is "+" else calc.sub(a, b)
-              if op is "-" else calc.mul(a, b)
-              if op is "*" else calc.div(a, b)
-              if op is "/" else 0)
-    print("{} {} {} = {}".format(a, op, b, result))
+    a = sys.argv[1]
+    b = sys.argv[3]
+    if operator == '+':
+        result = add(int(a), int(b))
+    elif operator == '-':
+        result = sub(int(a), int(b))
+    elif operator == '*':
+        result = mul(int(a), int(b))
+    else:
+        result = div(int(a), int(b))
+    print("{} {} {} = {}".format(a, operator, b, result))
