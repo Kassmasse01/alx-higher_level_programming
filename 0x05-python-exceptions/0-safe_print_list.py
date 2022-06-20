4e72b4d8b336bb10653e769c588d34fe7925c483
Freedom <freedom@localhost.localdomain>
2022-06-21 00:26:18 +0300

    exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        while count < x:
-            print("{}".format(my_list[count]), end='')
-            count += 1
-        print()
-        return count
-    except IndexError:
-        print()
-        return count
-        pass
+    """
+    Prints x elements from list
+    """
+    number = 0
+    for n in range(0, x):
+        try:
+            print('{}'.format(my_list[n]), end='')
+            number += 1
+        except:
+            break
+    print()
+    return number
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    co = 0
-    length = []
-    while co < list_length:
+    """
+    Divides element by elements 2 list
+    """
+    result = []
+    for i in range(0, list_length):
         try:
-            res = my_list_1[co] / my_list_2[co]
-        except ZeroDivisionError:
-            res = 0
-            print("division by 0")
-        except TypeError:
-            res = 0
-            print("wrong type")
+            result.append(my_list_1[i] / my_list_2[i])
         except IndexError:
-            res = 0
             print("out of range")
+            result.append(0)
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
         finally:
-            co += 1
-            length.append(res)
-    return length
-
+            pass
+    return result
