875962e9b3dca957deac9801e35e8ea08ba4f60d
Free'Dem <freedem@localhost.localdomain>
2022-09-15 04:43:28 +0300

    advanced

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-# delete all State objects with a name containing letter a
+"""script to create city and state object"""
 
 if __name__ == "__main__":
     from sqlalchemy.engine import create_engine
     from sqlalchemy.engine.url import URL
     from sqlalchemy.orm import Session
-    from model_state import Base, State
+    from relationship_city import City
+    from relationship_state import Base, State
     from sys import argv
 
     db = {'drivername': 'mysql+mysqldb',
@@ -20,6 +21,10 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    for query in session.query(State).filter(State.name.like('%a%')).all():
-        session.delete(query)
+    city = City(name='San Francisco')
+    state = State(name='California', cities=[city])
+
+    session.add(state)
+    session.add(city)
     session.commit()
+    session.close()
--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-# delete all State objects with a name containing letter a
+"""script to create city and state object"""
 
 if __name__ == "__main__":
     from sqlalchemy.engine import create_engine
     from sqlalchemy.engine.url import URL
     from sqlalchemy.orm import Session
-    from model_state import Base, State
+    from relationship_city import City
+    from relationship_state import Base, State
     from sys import argv
 
     db = {'drivername': 'mysql+mysqldb',
@@ -20,6 +21,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    for query in session.query(State).filter(State.name.like('%a%')).all():
-        session.delete(query)
-    session.commit()
+    for city, state in session.query(City, State)\
+                              .filter(City.state_id == State.id):
+        print("{}: {} -> {}".format(city.id, city.name, state.name))
+
+    session.close()
